f6810ef6423968e4eb89764bff94e5cfefd42481
EmilDafinov <emil.dafinov@appdirect.com>
2017-03-05 22:35:19 -0500

    Tested resolver

--- src/main/scala/com/github/emildafinov/ad/sdk/event/AppMarketEventResolver.scala
+++ src/main/scala/com/github/emildafinov/ad/sdk/event/AppMarketEventResolver.scala
@@ -1,27 +1,44 @@
 package com.github.emildafinov.ad.sdk.event
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.HttpMethods.POST
+import akka.http.scaladsl.model.{HttpRequest, RequestEntity}
+import akka.stream.Materializer
 import com.github.emildafinov.ad.sdk.payload.{ApiResult, EventJsonSupport}
+import com.typesafe.scalalogging.StrictLogging
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class AppMarketEventResolver extends EventJsonSupport {
+class AppMarketEventResolver(implicit 
+                             ec: ExecutionContext, 
+                             as: ActorSystem,
+                             ma: Materializer) extends EventJsonSupport with StrictLogging {
   /**
     * Calls the resolve event endpoint of the AppMarket in order to notify it that an event has been resolved
+    *
     * @param resolveEndpointBaseUrl Base Url of the AppMarket
-    * @param eventId the id of the event that was resolved
-    * @param eventProcessingResult the payload returned to the AppMarket
+    * @param eventId                the id of the event that was resolved
+    * @param eventProcessingResult  the payload returned to the AppMarket
     */
-  def sendEventResolvedCallback(resolveEndpointBaseUrl: String, eventId: String, eventProcessingResult: ApiResult): Future[Unit] = {
-//    Marshal(eventProcessingResult).to[RequestEntity]
-//      .flatMap { requestEntity =>
-//        Http().singleRequest(
-//          HttpRequest(
-//            method = POST,
-//            uri = s"$resolveEndpointBaseUrl/api/integration/v1/events/$eventId/result",
-//            entity = requestEntity
-//          )
-//        )
-//      }
-    ???
+  def sendEventResolvedCallback(resolveEndpointBaseUrl: String,
+                                eventId: String,
+                                eventProcessingResult: ApiResult): Future[Unit] = {
+
+    for {
+      requestEntity <- Marshal(eventProcessingResult).to[RequestEntity]
+      response <- Http().singleRequest(
+        HttpRequest(
+          method = POST,
+          uri = s"$resolveEndpointBaseUrl/api/integration/v1/events/$eventId/result",
+          entity = requestEntity
+        )
+      )
+    } yield
+      if (response.status.isSuccess)
+        logger.info(s"Successfully resolved event $eventId from AppMarket instance at $resolveEndpointBaseUrl")
+      else
+        logger.error(s"Failed sending a resolution message for event $eventId from AppMarket instance at $resolveEndpointBaseUrl")
   }
 }
---
+++ src/test/java/com/github/emildafinov/ad/sdk/event/AppMarketEventResolverTest.scala
@@ -0,0 +1,50 @@
+package com.github.emildafinov.ad.sdk.event
+
+import com.github.emildafinov.ad.sdk.payload.ApiResults
+import com.github.emildafinov.ad.sdk.{AkkaSpec, UnitTestSpec, WiremockHttpServiceTestSuite}
+import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, post, postRequestedFor, urlPathEqualTo}
+import org.scalatest.concurrent.PatienceConfiguration.Timeout
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.Span
+
+import scala.concurrent.duration.span._
+
+class AppMarketEventResolverTest extends UnitTestSpec with AkkaSpec with WiremockHttpServiceTestSuite {
+
+  behavior of "AppMarketEventResolver"
+
+  val tested = new AppMarketEventResolver()
+
+  it should "send the `event resolved` callback" in {
+    //Given
+    val testEventId = "1234qwer"
+    val testResolveEndpointUrl = dummyUrl
+    val testEventProcessingResult = ApiResults.unknownError()
+    httpServerMock.
+      givenThat(
+        post(urlPathEqualTo(s"/api/integration/v1/events/$testEventId/result")) willReturn {
+          aResponse().withStatus(200)
+        }
+      )
+
+    //When
+    val eventNotificatoinFuture = tested.sendEventResolvedCallback(
+      resolveEndpointBaseUrl = s"http://localhost:${httpServerMock.port()}",
+      eventId = testEventId,
+      eventProcessingResult = testEventProcessingResult
+    )
+
+    //Then
+    whenReady(
+      future = eventNotificatoinFuture,
+      timeout = Timeout(5 seconds)
+    ) { _ =>
+      httpServerMock
+        .verify(
+          postRequestedFor(
+            urlPathEqualTo(s"/api/integration/v1/events/$testEventId/result")
+          )
+        )
+    }
+  }
+}
