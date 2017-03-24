06e888de9349453e27e521ddad6c32f34ce5ab34
EmilDafinov <emil.dafinov@appdirect.com>
2017-03-24 18:18:06 -0400

    Individual constructors for the resolvers

---
+++ src/main/scala/com/github/emildafinov/ad/sdk/event/resolvers/SubscriptionOrderEventResolver.scala
@@ -0,0 +1,17 @@
+package com.github.emildafinov.ad.sdk.event.resolvers
+
+import com.github.emildafinov.ad.sdk.authentication.{AppMarketCredentialsSupplier, AuthorizationTokenGenerator}
+import com.github.emildafinov.ad.sdk.event.SdkProvidedEventResolver
+import com.github.emildafinov.ad.sdk.event.marshallers.SubscriptionOrderResponseMarshaller
+import com.github.emildafinov.ad.sdk.event.payloads.EventResolver
+import com.github.emildafinov.ad.sdk.event.responses.SubscriptionOrderResponse
+import com.github.emildafinov.ad.sdk.http.client.AppMarketEventResolver
+
+object SubscriptionOrderEventResolver {
+
+  def apply(credentialsSupplier: AppMarketCredentialsSupplier): EventResolver[SubscriptionOrderResponse] = {
+    val authorizationTokenGenerator = new AuthorizationTokenGenerator
+    val eventResolver = new AppMarketEventResolver(authorizationTokenGenerator, credentialsSupplier)
+    new SdkProvidedEventResolver(eventResolver, SubscriptionOrderResponseMarshaller())
+  }
+}
