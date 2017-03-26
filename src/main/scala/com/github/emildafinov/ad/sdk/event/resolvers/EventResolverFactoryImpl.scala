17f1b78d1079f568fc546a937ae86ff07e4e4f4c
EmilDafinov <emil.dafinov@appdirect.com>
2017-03-25 20:29:05 -0400

    ..

--- src/main/scala/com/github/emildafinov/ad/sdk/event/resolvers/EventResolverFactoryImpl.scala
+++ src/main/scala/com/github/emildafinov/ad/sdk/event/resolvers/EventResolverFactoryImpl.scala
@@ -6,6 +6,7 @@ import com.github.emildafinov.ad.sdk.internal.ClientDefinedCredentialsModule
 
 class EventResolverFactoryImpl(cs: AppMarketCredentialsSupplier) 
   extends ConnectorResolversAkkaDependenciesModule 
+    with EventResolversModule
     with ClientDefinedCredentialsModule {
   override val credentialsSupplier: AppMarketCredentialsSupplier = cs
 }
