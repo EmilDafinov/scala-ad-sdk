f579c03e4b30f66bfa49e5a3ed71b2d58c19666a
EmilDafinov <emil.dafinov@appdirect.com>
2017-07-03 13:11:41 -0400

    Added project URL, reformatted .travis.yml

--- src/scripts/assert_project_version_not_overriden.sh
+++ src/scripts/assert_project_version_not_overriden.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# Output current project version:
+sbt version
 # Errors out if the dynamically generated project version (based on the git version) is manually overriden sbt 
 sbt dynverAssertVersion
