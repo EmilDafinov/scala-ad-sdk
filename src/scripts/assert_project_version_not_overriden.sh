0fba949a7f5b94da808d1728d6dd6552a7e97e20
EmilDafinov <emil.dafinov@appdirect.com>
2017-07-06 21:14:00 -0400

    output tags

--- src/scripts/assert_project_version_not_overriden.sh
+++ src/scripts/assert_project_version_not_overriden.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-git tag
+echo "Tags on the current commit: "
+git tag --points-at HEAD
 # Output current project version:
 sbt version
 # Errors out if the dynamically generated project version (based on the git version) is manually overriden sbt 
