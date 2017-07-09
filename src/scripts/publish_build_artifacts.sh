a7b738cac3b73e9641dcebf91c9b668d08a2cd30
EmilDafinov <emil.dafinov@appdirect.com>
2017-07-09 07:36:29 -0400

    Ensure bintray credentials are present before publishing

--- src/scripts/publish_build_artifacts.sh
+++ src/scripts/publish_build_artifacts.sh
@@ -4,4 +4,4 @@
 curl -O https://gist.githubusercontent.com/EmilDafinov/c874e7f3abe77dd770c2bd26828b9224/raw/f75b659d1a2c3634140eace2c7899dc634e2790b/travis_bintray_credentials.sh &&
 chmod +x travis_bintray_credentials.sh &&
 ./travis_bintray_credentials.sh &&
-sbt ++$TRAVIS_SCALA_VERSION publish
+sbt ++$TRAVIS_SCALA_VERSION bintrayEnsureCredentials publish
